f77c91c35a6fbe0326bee8f2155cf1c85b34ebb3
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-01 11:40:19 +0530

    commit

--- Hiru_DB/lambda.js
+++ Hiru_DB/lambda.js
@@ -1,22 +1,17 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
-
-	// var insert = {};
-	// insert.id = String(event.id);
-	// insert.name = Sring(event.name);
-	// insert.role = String(event.role);
-
 	ddb.put({
-		TableName: 'Hiru_001',
-		Item: { 'ID': '001', 'Name': 'Hiru', 'JobRole': 'QA' }
+		TableName: 'Hiru001',
+		Item: { 'ID': '001', 'Name': 'Hiru', 'Role': 'QA' }
 	}, function (err, data) {
 		if (err) {
-			console.log(err);
+			console.log('error',err);
 		} else {
-			console.log(data);
+			console.log('data', data);
 		}
 	});
+
 
 	callback(null, 'Successfully executed');
 }
